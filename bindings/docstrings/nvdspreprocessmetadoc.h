e7acb190ac8d58d6e228851c2dbcb0dc151e0a11
pranjalnandi <123481546+pranjalnandi@users.noreply.github.com>
2023-03-21 13:09:21 +0600

    Update preprocess bindings docstrings (#1)
    Co-authored-by: Md Rayed Bin Wahed <rayed@therapservices.net>

--- bindings/docstrings/nvdspreprocessmetadoc.h
+++ bindings/docstrings/nvdspreprocessmetadoc.h
@@ -7,16 +7,15 @@ namespace pydsdoc
         namespace NvDsPreProcessTensorMetaDoc
         {
             constexpr const char* descr = R"pyds(
-                Holds information about preprocessed tensor metadata
+                Holds information about preprocess tensor metadata.
                 
-                :ivar raw_tensor_buffer: *void*, raw_tensor_buffer
-                :ivar buffer_size: *int*, buffer_size
-                :ivar tensor_shape: *vector*, tensor_shape
-                :ivar data_type: *NvDsDataType*, data_type
-                :ivar tensor_name: *string*, tensor_name
-                :ivar gpu_id: *int*, gpu_id
-                :ivar private_data: *void*, private_data
-                :ivar meta_id: *int*, meta_id)pyds";
+                :ivar raw_tensor_buffer: Holds raw tensor buffer preprocessed for infer.
+                :ivar buffer_size: *int*, Size of raw tensor buffer.
+                :ivar tensor_shape: *list of int*, Shape of raw tensor buffer.
+                :ivar data_type: :class:`NvDsDataType`, Model datatype for which tensor prepared.
+                :ivar tensor_name: *string*, Model input layer name.
+                :ivar gpu_id: *int*, gpu-id on which tensor prepared.
+                :ivar private_data: *void* Private data used for the meta producer's internal memory management.)pyds";
             
             constexpr const char* cast=R"pyds(cast given object/data to :class:`NvDsPreProcessTensorMeta`, call pyds.NvDsPreProcessTensorMeta.cast(data))pyds";
         }
@@ -24,12 +23,27 @@ namespace pydsdoc
         namespace GstNvDsPreProcessBatchMetaDoc
         {
             constexpr const char* descr = R"pyds(
-                Holds information about preprocess batch metadata
+                Holds information about preprocess batch metadata.
                 
-                :ivar target_unique_ids: *int*, target_unique_ids
-                :ivar tensor_meta: *NvDsPreProcessTensorMeta*, tensor_meta
-                :ivar roi_vector: *vector*, roi_vector
-                :ivar private_data: *void*, private_data)pyds";
+                :ivar target_unique_ids: *int*, Holds the list of component gie-id for which tensor is prepared.
+                :ivar tensor_meta: Holds a pointer to object of type :class: `NvDsPreProcessTensorMeta`.
+                :ivar roi_vector: *list of type NvdsRoiMeta*, Holds the list of RoiMeta per batch.
+                :ivar private_data: *void* Private data used for the meta producer's internal memory management.
+                
+                Example Usage:
+                ::
+
+                    user_meta_list = batch_meta.batch_user_meta_list # Retrieve list of NvDsUserMeta objects in use in the current batch. See NvDsUserMeta documentation for more details.
+
+                    while user_meta_list is not None:
+                        try:
+                            user_meta = pyds.NvDsUserMeta.cast(user_meta_list.data)
+                        except StopIteration:
+                            break
+
+                        if user_meta.base_meta.meta_type == pyds.NVDS_PREPROCESS_BATCH_META:
+                            # Casting user_meta.user_meta_data to pyds.GstNvDsPreProcessBatchMeta
+                            preprocess_batch_meta = pyds.GstNvDsPreProcessBatchMeta.cast(user_meta.user_meta_data))pyds";
             
             constexpr const char* cast=R"pyds(cast given object/data to :class:`GstNvDsPreProcessBatchMeta`, call pyds.GstNvDsPreProcessBatchMeta.cast(data))pyds";
         }
--- bindings/docstrings/nvdsroimetadoc.h
+++ bindings/docstrings/nvdsroimetadoc.h
@@ -6,39 +6,46 @@ namespace pydsdoc
     {
         namespace NvDsDataTypeDoc
         {
-            constexpr const char* descr = R"pyds(Specifies the type of NVIDIA defined NvDsDataType)pyds";
-            constexpr const char* NvDsDataType_FP32=R"pyds(FP32)pyds";
-            constexpr const char* NvDsDataType_UINT8=R"pyds(UINT8)pyds";
-            constexpr const char* NvDsDataType_INT8=R"pyds(INT8)pyds";
-            constexpr const char* NvDsDataType_UINT32=R"pyds(UINT32)pyds";
-            constexpr const char* NvDsDataType_INT32=R"pyds(INT32)pyds";
-            constexpr const char* NvDsDataType_FP16=R"pyds(FP16)pyds";
+            constexpr const char* descr = R"pyds(
+                *Enumerator*. Specifies the type of meta data. 
+                NvDsDataType will be present in the range from NvDsDataType_FP32 to NvDsDataType_FP16.
+                User can add its own metadata type NvDsDataType_FP16 onwards.)pyds";
+            
+            constexpr const char* NvDsDataType_FP32=R"pyds(metadata type to be set for FP32 datatype)pyds";
+            constexpr const char* NvDsDataType_UINT8=R"pyds(metadata type to be set for UINT8 datatype)pyds";
+            constexpr const char* NvDsDataType_INT8=R"pyds(metadata type to be set for INT8 datatype)pyds";
+            constexpr const char* NvDsDataType_UINT32=R"pyds(metadata type to be set for UINT32 datatype)pyds";
+            constexpr const char* NvDsDataType_INT32=R"pyds(metadata type to be set for INT32 datatype)pyds";
+            constexpr const char* NvDsDataType_FP16=R"pyds(metadata type to be set for FP16 datatype)pyds";
         }
 
         namespace NvDsUnitTypeDoc
         {
-            constexpr const char* descr = R"pyds(Specifies the type of nvNVIDIA defined NvDsUnitType)pyds";
-            constexpr const char* NvDsUnitType_FullFrame=R"pyds(FullFrame)pyds";
-            constexpr const char* NvDsUnitType_ROI=R"pyds(ROI)pyds";
-            constexpr const char* NvDsUnitType_Object=R"pyds(Object)pyds";
+            constexpr const char* descr = R"pyds(
+                *Enumerator*. Specifies the type of meta data. 
+                NvDsUnitType will be present in the range from NvDsUnitType_FullFrame to NvDsDataType_Object.
+                User can add its own metadata type NvDsUnitType_Object onwards.)pyds";
+            
+            constexpr const char* NvDsUnitType_FullFrame=R"pyds(metadata type to be set for Full Frames)pyds";
+            constexpr const char* NvDsUnitType_ROI=R"pyds(metadata type to be set for Region of Interests)pyds";
+            constexpr const char* NvDsUnitType_Object=R"pyds(metadata type to be set for Cropped Objects)pyds";
         }
         
         namespace NvDsRoiMetaDoc
         {          
             constexpr const char* descr = R"pyds(
-                Holds information about roi metadata
+                Holds information about roi metadata.
                 
-                :ivar roi: :class:`NvOSD_RectParams`,
-                :ivar converted_buffer: :class:`NvBufSurfaceParams`,
-                :ivar frame_meta: :class:`NvDsFrameMeta`,
-                :ivar object_meta: :class:`NvDsObjectMeta`,
-                :ivar classifier_meta_list: :class:`NvDsClassifierMetaList`,
-                :ivar roi_user_meta_list: :class:`NvDsUserMetaList`,
-                :ivar scale_ratio_x: :class:`gdouble`,
-                :ivar scale_ratio_y: :class:`gdouble`,
-                :ivar offset_left: :class:`gdouble`,                
-                :ivar offset_top: :class:`gdouble`,
-                :ivar roi_polygon: :class:`roi_polygon`
+                :ivar roi: :class:`NvOSD_RectParams`, Structure containing the positional parameters of the object in the frame.
+                :ivar converted_buffer: :class:`NvBufSurfaceParams`.
+                :ivar frame_meta: An object of type :class:`NvDsFrameMeta`.
+                :ivar object_meta: An object of type :class:`NvDsObjectMeta`.
+                :ivar classifier_meta_list: Holds a pointer to a list of pointers of type :class:`NvDsClassifierMeta`.
+                :ivar roi_user_meta_list: Holds a pointer to a list of pointers of type :class:`NvDsUserMeta`.
+                :ivar scale_ratio_x: :class:`gdouble`, Ratio by which the frame/ROI crop was scaled in horizontal direction Required when scaling co-ordinates/sizes in metadata back to input resolution.
+                :ivar scale_ratio_y: :class:`gdouble`, Ratio by which the frame/ROI crop was scaled in vertical direction Required when scaling co-ordinates/sizes in metadata back to input resolution.
+                :ivar offset_left: :class:`gdouble`, offsets in horizontal direction while scaling.                
+                :ivar offset_top: :class:`gdouble`, offsets in vertical direction while scaling.
                 )pyds";
 
             constexpr const char* cast=R"pyds(cast given object/data to :class:`NvDsRoiMeta`, call pyds.NvDsRoiMeta.cast(data))pyds";
